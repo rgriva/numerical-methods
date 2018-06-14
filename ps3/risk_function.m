39d822bc2bdc0c7af04d3f6cb6b207bbef600c45
Raul Guarini <raul.guarini@gmail.com>
2018-06-14 12:06:12 -0300

    Corrected documentation for risk_function

--- ps3/risk_function.m
+++ ps3/risk_function.m
@@ -2,7 +2,7 @@ function risk_vector = risk_function(gamma, K0, kgrid, zgrid, state, P, alpha, m
 % This computes the risk function! The output is a column vector of
 % R(gamma, K0), where K0 are collocation points.
 % gamma: projection coefficients
-% K0: collocation points
+% K0: capital points
 % kgrid: grid points for capital
 % zgrid: grid points for log(TFP)
 % state: current log(TFP) state index
